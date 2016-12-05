aa0c7fdb57f4cc50398abe13f060220a5dc817e3
cescoto <caescoto616@gmail.com>
2016-12-05 00:11:34 -0600

    Added a function to pull a random character's name when button is pressed, and then appends it to the ul

--- js/swapi.js
+++ js/swapi.js
@@ -1,11 +1,21 @@
 $.noConflict();
 (function($){
   $(document).ready(function() {
+    var $chars = $('#character');
     function generate() {
       $number = 1 + Math.floor(Math.random() * 87);
     }
     setInterval(generate,1000);
-    var $chars = $('#character');
+
+    $(function(){
+      $("#random").click(function(){
+        $.getJSON('http://swapi.co/api/people/' + $number,
+        function(data) {
+          $chars.empty();
+          $chars.append('<li> Name: ' + data.name + '</li>');
+        });
+      });
+    });
   }
 );
 })(jQuery);
