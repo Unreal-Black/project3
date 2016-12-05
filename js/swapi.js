5bf96989d18eee10f2d3d1faea309e7a819ca488
cescoto <caescoto616@gmail.com>
2016-12-04 23:56:23 -0600

    Added a random number generator function

--- js/swapi.js
+++ js/swapi.js
@@ -1,6 +1,10 @@
 $.noConflict();
 (function($){
   $(document).ready(function() {
+    function generate() {
+      $number = 1 + Math.floor(Math.random() * 87);
+    }
+    setInterval(generate,1000);
   }
 );
 })(jQuery);
