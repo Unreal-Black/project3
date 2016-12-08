da711f1dc60763e3939c4de0d60ad7917f7c0861
cescoto <caescoto616@gmail.com>
2016-12-07 22:04:08 -0600

    Added a function that is part of the requirements for the project

--- js/swapi.js
+++ js/swapi.js
@@ -1,26 +1,44 @@
 $.noConflict();
 (function($){
   $(document).ready(function() {
-    var $chars = $('#character');
+    var $bio = $('#bio');
+
     function generate() {
-      $number = 1 + Math.floor(Math.random() * 87);
+       $number = 1 + Math.floor(Math.random() * 87);
     }
     setInterval(generate,1000);
 
-    $(function(){
-      $("#random").click(function(){
-        $.getJSON('https://swapi.co/api/people/' + $number,
-        function(data) {
-          $chars.empty();
-          $chars.append('<li> Name: ' + data.name + '</li>');
-          $chars.append('<li> Birth year: ' + data.birth_year + '</li>');
-          $chars.append('<li> Height: ' + data.height + ' cm </li>');
-          $chars.append('<li> Weight: ' + data.mass + ' kg </li>');
-          $chars.append('<li> Gender: ' + data.gender + '</li>');
-          $chars.append('<li> Hair color: ' + data.hair_color + '</li>');
-          $chars.append('<li> Skin color: ' + data.skin_color +'</li>');
-          $chars.append('<li> Eye color: ' + data.eye_color + '</li>');
+    $('#swapi-form').on('submit', function(e) {
+      var $char = $('#chars').val();
+      $.getJSON('https://swapi.co/api/people/?search=' + $char,
+      function(info) {
+        $bio.empty();
+        $bio.append('<li> Name: ' + info.results[0].name + '</li>');
+        $bio.append('<li> Birth year: ' + info.results[0].birth_year + '</li>');
+        $bio.append('<li> Height: ' + info.results[0].height + ' cm </li>');
+        $bio.append('<li> Weight: ' + info.results[0].mass + ' kg </li>');
+        $bio.append('<li> Gender: ' + info.results[0].gender + '</li>');
+        $bio.append('<li> Hair color: ' + info.results[0].hair_color + '</li>');
+        $bio.append('<li> Skin color: ' + info.results[0].skin_color +'</li>');
+        $bio.append('<li> Eye color: ' + info.results[0].eye_color + '</li>');
+        console.log(char);
+      });
+    });
 
+
+    $(function(){
+      $('#random').click(function(){
+        $.getJSON('http://swapi.co/api/people/' + $number,
+        function(data) {
+          $bio.empty();
+          $bio.append('<li> Name: ' + data.name + '</li>');
+          $bio.append('<li> Birth year: ' + data.birth_year + '</li>');
+          $bio.append('<li> Height: ' + data.height + ' cm </li>');
+          $bio.append('<li> Weight: ' + data.mass + ' kg </li>');
+          $bio.append('<li> Gender: ' + data.gender + '</li>');
+          $bio.append('<li> Hair color: ' + data.hair_color + '</li>');
+          $bio.append('<li> Eye color: ' + data.eye_color + '</li>');
+          $bio.append('<li> Skin color: ' + data.skin_color +'</li>');
         });
       });
     });
